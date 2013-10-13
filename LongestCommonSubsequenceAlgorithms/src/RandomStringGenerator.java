c8663d9855680ad643e8d7c4dcade28e6b684e7b
Jason A Smith <jas7553@rit.edu>
2013-10-13 18:49:12 -0500

    implementation of a random string generator Iterator

---
+++ LongestCommonSubsequenceAlgorithms/src/RandomStringGenerator.java
@@ -0,0 +1,64 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class RandomStringGenerator implements Iterator<String> {
+
+	private char[] alphabet;
+	private int size = 5;
+	private final Random randomNumberGenerator;
+
+	public RandomStringGenerator(char[] alphabet) {
+		this.alphabet = alphabet;
+		randomNumberGenerator = new Random();
+	}
+
+	public RandomStringGenerator(char[] alphabet, int seed) {
+		this.alphabet = alphabet;
+		randomNumberGenerator = new Random(seed);
+	}
+
+	public void setStringSize(int size) {
+		this.size = size;
+	}
+
+	public void setAlphabet(char[] alphabet) {
+		this.alphabet = alphabet;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return true;
+	}
+
+	@Override
+	public String next() {
+		StringBuilder s = new StringBuilder();
+		for (int i = 0; i < size; i++) {
+			int index = randomNumberGenerator.nextInt(alphabet.length);
+			s.append(alphabet[index]);
+		}
+
+		return s.toString();
+	}
+
+	@Override
+	public void remove() {
+	}
+
+	public static void main(String[] args) {
+		char[] binaryAlphabet = new char[] { '0', '1' };
+		char[] letterAlphabet = new char[] { 'A', 'C', 'G', 'T' };
+
+		RandomStringGenerator generator = new RandomStringGenerator(letterAlphabet);
+		for (int i = 0; i < 6; i++) {
+			System.out.println(generator.next());
+		}
+
+		System.out.println();
+
+		generator.setAlphabet(binaryAlphabet);
+		for (int i = 0; i < 6; i++) {
+			System.out.println(generator.next());
+		}
+	}
+}
