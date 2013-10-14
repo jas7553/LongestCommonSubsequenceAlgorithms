a30c99b54fd8d3f362b7651f62c445b838d18a2e
Jason A Smith <jas7553@rit.edu>
2013-10-13 19:20:06 -0500

    added test harness class, LCS

---
+++ LongestCommonSubsequenceAlgorithms/src/LCS.java
@@ -0,0 +1,38 @@
+/**
+ * @author Jason A Smith <jas7553>
+ * 
+ */
+public class LCS {
+
+	public static void main(String[] args) {
+		LcsSolver solver = new NaiveRecursive();
+
+		char[] alphabet = new char[] { 'A', 'C', 'G', 'T' };
+		RandomStringGenerator generator = new RandomStringGenerator(alphabet);
+
+		int iterations = 100;
+		int minLength = 1;
+		int maxLength = 15;
+
+		System.out.println("String size\tAvg recursive calls\tAvg elapsed time");
+
+		for (int i = minLength; i <= maxLength; i++) {
+			generator.setStringSize(i);
+
+			int totalRecursiveCalls = 0;
+			long totalTimeElapsed = 0;
+
+			for (int j = 0; j < iterations; j++) {
+				solver.setXY(generator.next(), generator.next());
+				solver.lcs();
+				totalRecursiveCalls += solver.getPerformanceMonitor().getRecursiveCallCount();
+				totalTimeElapsed += solver.getPerformanceMonitor().getElapsedTimeMillis();
+			}
+
+			int averageRecursiveCalls = totalRecursiveCalls / iterations;
+			long averageElapsedTime = totalTimeElapsed / iterations;
+
+			System.out.println(i + "\t" + averageRecursiveCalls + "\t" + averageElapsedTime);
+		}
+	}
+}
