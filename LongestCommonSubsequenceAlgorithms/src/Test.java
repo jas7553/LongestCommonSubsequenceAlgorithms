a76c226b792de36ea409124f325cb89b8c58a3f5
Jason A Smith <jas7553@rit.edu>
2013-10-13 20:00:16 -0500

    Test.java now throws a runtime exception if the solvers don't agree on an answer

--- LongestCommonSubsequenceAlgorithms/src/Test.java
+++ LongestCommonSubsequenceAlgorithms/src/Test.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Jason A Smith <jas7553>
  * 
@@ -5,13 +8,20 @@
 public class Test {
 
 	private LcsSolver[] solvers;
+	private RandomStringGenerator generator;
 
 	public Test() {
 		solvers = new LcsSolver[] { new NaiveRecursive(), new RecursiveMemoization(), new DynamicProgramming() };
+		generator = new RandomStringGenerator(new char[] { 'A', 'C', 'G', 'T' }, 10);
 	}
 
-	public void test(String x, String y) {
+	public void test() {
+		String x = generator.next();
+		String y = generator.next();
+
 		System.out.println("Testing with \"" + x + "\", \"" + y + "\"");
+
+		Set<String> answers = new HashSet<String>();
 
 		for (LcsSolver solver : solvers) {
 			solver.setXY(x, y);
@@ -20,13 +30,19 @@ public class Test {
 			int lcsLength = solver.lcsLength();
 
 			System.out.println(lcs + " " + lcsLength);
+			answers.add(lcs);
+		}
+
+		if (answers.size() != 1) {
+			throw new RuntimeException("An answer didn't match!");
 		}
 	}
 
 	public static void main(String... args) {
 		Test tester = new Test();
-		tester.test("AGGTAB", "GXTXAYB");
-		tester.test("ABCBDAB", "BDCABA");
-		tester.test("AAABBB", "CCAAACCBBBCC");
+		for (int i = 0; i < 50; i++) {
+			tester.test();
+			System.out.println();
+		}
 	}
 }
