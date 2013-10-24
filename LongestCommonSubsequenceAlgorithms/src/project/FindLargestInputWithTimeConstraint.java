6125b03508b7f1f2f410b8373c129d7c90a2704c
Jason A Smith <jas7553@rit.edu>
2013-10-24 11:34:08 -0400

    Initial commit of higher level script to test length of possible LCS in under 10sec

---
+++ LongestCommonSubsequenceAlgorithms/src/project/FindLargestInputWithTimeConstraint.java
@@ -0,0 +1,48 @@
+package project;
+
+import java.io.IOException;
+
+import algorithms.Hirschberg;
+import algorithms.LcsSolver;
+
+public class FindLargestInputWithTimeConstraint {
+
+	private static final long TIME_LIMIT_MS = 10 * 1000;
+
+	private RandomStringGenerator generator = new RandomStringGenerator(new char[] { 'A', 'C', 'G', 'T' });
+
+	public int test(LcsSolver solver, int initialInputSize, int delta) throws IOException {
+		long time = 0;
+		int inputSize = initialInputSize;
+
+		System.out.println(solver.getClass().getSimpleName());
+		System.out.println("Input size\tTime (ms)");
+		while (time < TIME_LIMIT_MS) {
+			inputSize += delta;
+			generator.setStringSize(inputSize);
+
+			solver.lcs(generator.next(), generator.next());
+			time = solver.getPerformanceMonitor().getElapsedTimeMillis();
+
+			System.out.println(inputSize + "\t" + time);
+		}
+
+		return inputSize;
+	}
+
+	public static void main(String[] args) throws IOException {
+		FindLargestInputWithTimeConstraint tester = new FindLargestInputWithTimeConstraint();
+
+//		tester.test(new NaiveRecursive(), 10, 1);
+//		System.out.println();
+
+//		tester.test(new RecursiveMemoization(), 400, 10);
+//		System.out.println();
+
+//		tester.test(new DynamicProgramming(), 3800);
+//		System.out.println();
+
+		tester.test(new Hirschberg(), 26000, 100);
+//		System.out.println();
+	}
+}
